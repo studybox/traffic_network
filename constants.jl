e470a86040aa9283c28a6b33f193074aa048666c
studybox <christy521515@gmail.com>
2018-07-20 16:02:33 -0400

    Add files via upload

--- constants.jl
+++ constants.jl
@@ -1,6 +1,7 @@
 SUMO = "sumo"
 SUMOGUI = "sumo-gui"
 SUMO_HOME = "/media/mSATA/UM/Simulation/sumo"
+SUMO_CONFIG = "/media/mSATA/UM/Simulation/f_reduced2_AnnArbor.sumocfg"
 unshift!(PyVector(pyimport("sys")["path"]), os.path[:join](SUMO_HOME, "tools"))
 #XBOUNDLEFT = 265395
 #XBOUNDRIGHT = 281180
@@ -10,3 +11,12 @@ XBOUNDLEFT = 262000
 XBOUNDRIGHT = 291000
 YBOUNDTOP = 4694500
 YBOUNDBOT = 4670500
+BACKGROUND_FILE = "/media/mSATA/UM/Upper routing simulation/SUMOdata/f_background2.png"
+CARICONS = [
+   "/home/boqi/Pictures/noun_Car_1822714_000000.png",
+   "/home/boqi/Pictures/noun_Car_1822714_51A7F9.png",
+   "/home/boqi/Pictures/noun_Car_1822714_70c1b3.png",
+   "/home/boqi/Pictures/noun_Car_1822714_70C041.png",
+   "/home/boqi/Pictures/noun_Car_1822714_B36AE2.png",
+   "/home/boqi/Pictures/noun_Car_1822714_EC5D57.png",
+   "/home/boqi/Pictures/noun_Car_1822714_F38F19.png"]
